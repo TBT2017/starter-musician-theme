a9da841575d5862788ffd2848a28873c04e0fc7a
TBT2017 <54323872+TBT2017@users.noreply.github.com>
2019-08-20 15:38:51 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,9 +7,9 @@
 module.exports = {
   // Add common site data (not required by the theme, but recommended)
   siteMetadata: {
-    title: "Starter Site for Gatsby Theme Musician",
+    title: "The Bluebird Two",
     description: "",
-    siteUrl: "https://github.com/ekafyi/starter-musician-theme"
+    siteUrl: "https://thebluebirdtwo.netlify.com"
   },
   plugins: [
     // Add gatsby-theme-musician to your site
